4bc8b3332031fab4cab3c5bbc70111b35d9436f9
rinel <rineliniguezsosa@gmail.com>
2023-03-20 15:28:14 -0600

    agregando el contenido del formulario

--- src/useFormik/Registroform.js
+++ src/useFormik/Registroform.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Registroform = () => {
   return (
-    <div>Registroform</div>
+    <>
+    <form style={{width:'50%'}}>
+        <div className="mb-3">
+            <input placeholder="Nombre" name="nombre" type="text" className="form-control" id="nombre" />
+        </div>
+        <div className="mb-3">
+            <input placeholder='email' nombre="email" type="email" className="form-control" id="email"/>
+        </div>
+        <div className="mb-3">
+            <input type="password" name="password" placeholder='password' className="form-control" id="password"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    </>
   )
 }
