fbb7b75bc5172c3a04c5c81f4b2d6ac5810ebbab
rinel <rineliniguezsosa@gmail.com>
2023-03-20 22:10:52 -0600

    creando el archivo Regostroformcopy

---
+++ src/Reducing Boilerplate/Registroformcopy.js
@@ -0,0 +1,61 @@
+import { useFormik } from 'formik'
+ import * as Yup from 'yup';
+
+export const Registroformcopy = () => {
+
+    const formik = useFormik({
+        initialValues:{ 
+            nombre:'',
+            email:'',
+            password:''
+        },
+        onSubmit:(values)=>{
+            console.log("Datos: ",values)
+        },
+        //validate,
+        validationSchema: Yup.object({
+            nombre: Yup.string().required('requerido!')
+        })
+    })
+
+    console.log(formik.touched)
+  return (
+    <>
+    <form onSubmit={formik.handleSubmit} style={{width:'50%'}}>
+        <div>
+        <div className="mb-3">
+            <input placeholder="Nombre" 
+            name="nombre" 
+            onChange={formik.handleChange} 
+            value={formik.values.nombre} 
+            onBlur={formik.handleBlur}
+            type="text" 
+            className="form-control" 
+            id="nombre" />
+            {formik.touched.nombre && formik.errors.nombre ? <div style={{color:'red',fontSize:'10px'}}>{formik.errors.nombre}</div>: null}
+        </div>
+        </div>
+        <div className="mb-3">
+            <input placeholder='email' 
+            nombre="email" 
+            type="email" 
+            className="form-control" 
+            onChange={formik.handleChange} 
+            value={formik.values.email}
+            id="email"/>
+            
+        </div>
+        <div className="mb-3">
+            <input type="password" 
+            name="password" 
+            placeholder='password' 
+            className="form-control" 
+            onChange={formik.handleChange} 
+            value={formik.values.password}
+            id="password"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    </>
+  )
+}
